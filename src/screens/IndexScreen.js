3cad1178182182cebd95025b9624be2b51e65106
Prawee Wongsa <prawee@hotmail.com>
2019-11-23 02:39:01 +0700

    update new value of context

--- src/screens/IndexScreen.js
+++ src/screens/IndexScreen.js
@@ -3,14 +3,15 @@ import { View, Text, StyleSheet, FlatList, Button } from 'react-native'
 import BlogContext from '../context/BlogContext'
 
 const IndexScreen = () => {
-  const blogPosts = useContext(BlogContext)
+  // const blogPosts = useContext(BlogContext)
+  const { data, addBlogPost } = useContext(BlogContext)
 
   return (
     <View>
       <Text>Index Screen</Text>
       <Button title="Add Post" />
       <FlatList 
-        data={blogPosts}
+        data={data}
         keyExtractor={blogPost => blogPost.title}
         renderItem={({ item }) => {
           return <Text>{item.title}</Text>
