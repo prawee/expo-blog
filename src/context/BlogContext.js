388a2c72e2878455a4def747a436e26445595b1d
Prawee Wongsa <prawee@hotmail.com>
2019-11-23 02:10:20 +0700

    define value of provider and export default context

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -2,11 +2,12 @@ import React from 'react'
 
 const BlogContext = React.createContext()
 
-
 export const BlogProvider = ({ children }) => {
   return (
-    <BlogContext.Provider>
+    <BlogContext.Provider value={5}>
       {children}
     </BlogContext.Provider>
   )
 }
+
+export default BlogContext
