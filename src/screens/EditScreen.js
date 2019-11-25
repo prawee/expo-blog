4b19fffc87c097c0ad4683fca82b0a3a54b0055c
Prawee Wongsa <prawee@hotmail.com>
2019-11-26 01:00:12 +0700

    testing using context and show id param

--- src/screens/EditScreen.js
+++ src/screens/EditScreen.js
@@ -1,7 +1,10 @@
-import React from 'react'
+import React, { useState, useContext } from 'react'
 import { View, Text, StyleSheet } from 'react-native'
+import { Context } from '../context/BlogContext'
 
 const EditScreen = ({ navigation }) => {
+  const { state } = useContext(Context)
+
   return (
     <View>
       <Text>Edit Screen - {navigation.getParam('id')}</Text>
