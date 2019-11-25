a9456ca8f22a9b34c9cc9a2ef693d47eac91582c
Prawee Wongsa <prawee@hotmail.com>
2019-11-26 01:22:22 +0700

    import blog post from to create screen

--- src/screens/CreateScreen.js
+++ src/screens/CreateScreen.js
@@ -1,10 +1,12 @@
 import React, { useContext, useState } from 'react'
 import { View, Text, TextInput, Button, StyleSheet } from 'react-native'
 import { Context } from '../context/BlogContext'
+import BlogPostForm from '../components/BlogPostForm'
 
 const CreateScreen = ({ navigation }) => {
   const { addBlogPost } = useContext(Context)
 
+  return <BlogPostForm />
 }
 
 const styles = StyleSheet.create({
