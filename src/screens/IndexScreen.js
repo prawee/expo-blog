ae75dba68ea7f9ddc0d121f7d7b3335dfd63abe0
Prawee Wongsa <prawee@hotmail.com>
2019-11-25 16:30:40 +0700

    using touchable with row for show screen

--- src/screens/IndexScreen.js
+++ src/screens/IndexScreen.js
@@ -3,7 +3,7 @@ import { View, Text, StyleSheet, FlatList, Button, TouchableOpacity } from 'reac
 import { Context } from '../context/BlogContext'
 import { Feather } from '@expo/vector-icons'
 
-const IndexScreen = () => {
+const IndexScreen = ({ navigation }) => {
   // const blogPosts = useContext(BlogContext)
   const { state, addBlogPost, deleteBlogPost } = useContext(Context)
 
@@ -15,12 +15,15 @@ const IndexScreen = () => {
         keyExtractor={blogPost => blogPost.title}
         renderItem={({ item }) => {
           return (
-            <View style={styles.row}>
-              <Text style={styles.title}>{item.title} - {item.id}</Text>
-              <TouchableOpacity onPress={() => deleteBlogPost(item.id)}>
-                <Feather style={styles.icon} name="trash" />
-              </TouchableOpacity> 
-            </View>
+            <TouchableOpacity 
+              onPress={() => navigation.navigate('Show', { id: item.id })}>
+              <View style={styles.row}>
+                <Text style={styles.title}>{item.title} - {item.id}</Text>
+                <TouchableOpacity onPress={() => deleteBlogPost(item.id)}>
+                  <Feather style={styles.icon} name="trash" />
+                </TouchableOpacity> 
+              </View>
+            </TouchableOpacity>
           )
         }}
       />
