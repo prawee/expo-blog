034692d8a737ea624637b22eec87a7f7129e317f
Prawee Wongsa <prawee@hotmail.com>
2019-11-26 01:24:15 +0700

    import and using blog post form instead

--- src/screens/EditScreen.js
+++ src/screens/EditScreen.js
@@ -1,6 +1,7 @@
 import React, { useState, useContext } from 'react'
 import { View, Text, StyleSheet, TextInput } from 'react-native'
 import { Context } from '../context/BlogContext'
+import BlogPostForm from '../components/BlogPostForm'
 
 const EditScreen = ({ navigation }) => {
   const { state } = useContext(Context)
@@ -10,20 +11,7 @@ const EditScreen = ({ navigation }) => {
   const [title, setTitle] = useState(blogPost.title)
   const [content, setContent] = useState(blogPost.content)
 
-  return (
-    <View>
-      <Text>Edit Title:</Text>
-      <TextInput 
-        value={title}
-        onChangeText={newTitle => setTitle(newTitle)}
-      />
-      <Text>Edit Content:</Text>
-      <TextInput 
-        value={content}
-        onChangeText={newContent => setContent(newContent)}
-      />
-    </View>
-  )
+  return <BlogPostForm />
 }
 
 const styles = StyleSheet.create({})
