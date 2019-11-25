f983d185c49419065d4f52dae70c942ccf213c5d
Prawee Wongsa <prawee@hotmail.com>
2019-11-26 01:02:43 +0700

    using state with default blog post

--- src/screens/EditScreen.js
+++ src/screens/EditScreen.js
@@ -7,6 +7,9 @@ const EditScreen = ({ navigation }) => {
 
   const blogPost = state.find(blogPost => blogPost.id === navigation.getParam('id'))
 
+  const [title, setTitle] = useState(blogPost.title)
+  const [content, setContent] = useState(blogPost.content)
+
   return (
     <View>
       <Text>Edit Screen - {navigation.getParam('id')}</Text>
