8fb7928765d75a88b6f6b5c993da75582487dbfe
Prawee Wongsa <prawee@hotmail.com>
2019-11-26 01:25:16 +0700

    remove state on edit screen

--- src/screens/EditScreen.js
+++ src/screens/EditScreen.js
@@ -1,4 +1,4 @@
-import React, { useState, useContext } from 'react'
+import React, { useContext } from 'react'
 import { View, Text, StyleSheet, TextInput } from 'react-native'
 import { Context } from '../context/BlogContext'
 import BlogPostForm from '../components/BlogPostForm'
@@ -7,9 +7,6 @@ const EditScreen = ({ navigation }) => {
   const { state } = useContext(Context)
 
   const blogPost = state.find(blogPost => blogPost.id === navigation.getParam('id'))
-
-  const [title, setTitle] = useState(blogPost.title)
-  const [content, setContent] = useState(blogPost.content)
 
   return <BlogPostForm />
 }
