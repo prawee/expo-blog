8f0c7655e0b8f2c38e8434c77cd95d2946f82cb5
Prawee Wongsa <prawee@hotmail.com>
2019-11-27 01:12:59 +0700

    update editBlogPost action with jsonServer

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -54,7 +54,8 @@ const deleteBlogPost = dispatch => {
 }
 
 const editBlogPost = dispatch => {
-  return (id, title, content, callback) => {
+  return async (id, title, content, callback) => {
+    await jsonServer.put(`/blogposts/${id}`, { title, content })
     dispatch({ type: 'edit_blogpost', payload: { id, title, content} })
     if (callback) {
       callback()
